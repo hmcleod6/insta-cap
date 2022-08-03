25b4a536d89048c835d9cee71ece95891489bb78
Heather <hmcleod29@gmail.com>
2022-08-03 13:38:00 -0400

    Finish app without styline

--- insta-fun/src/components/CaptionList.js
+++ insta-fun/src/components/CaptionList.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./CaptionList.css";
 
 export default function CaptionList({ data }) {
-  return <form>{data.forEach((element) => console.log(element))}</form>;
+  const arrayLength = data[0].length;
+  const randomCaptionNumber = Math.floor(Math.random() * arrayLength);
+
+  return (
+    <form>
+      <div className="random-caption">{data[0][randomCaptionNumber]}</div>
+    </form>
+  );
 }
